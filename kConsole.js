4c575c7110bfc7ccd7dc583fb11800861b285ce0
Keillion <735699921@qq.com>
2017-09-15 11:26:46 +0800

    clear button

--- kConsole.js
+++ kConsole.js
@@ -1,10 +1,13 @@
 $('body').append($(['<div>',
-	'<div id="kConsoleLogDiv" style="display:none;position:fixed;top:0;right:0;width:100%;height:80%;overflow-y:auto;font-size:16px;line-height:24px;work-break:break-all;word-wrap:break-word;background-color:rgba(0,0,0,0.5);color:white;box-sizing:border-box;z-index:32767;"></div>',
+	'<div id="kConsoleLogDiv" style="display:none;position:fixed;top:0;right:0;width:100%;height:80%;padding:16px 0;overflow-y:scroll;font-size:16px;line-height:24px;work-break:break-all;word-wrap:break-word;background-color:rgba(0,0,0,0.5);color:white;box-sizing:border-box;z-index:32767;"></div>',
 	'<div style="display:none;width:100%;height:20%;font-size:16px;line-height:24px;position:fixed;bottom:0;work-break:break-all;word-wrap:break-word;background-color:rgba(0,0,0,0.5);box-sizing:border-box;z-index:32767;">',
 		'<textarea id="kConsoleTextArea" type="text" style="display:inline-block;width:90%;height:100%;box-sizing:border-box;"></textarea>',
 		'<button id="kConsoleBtnRun" style="display:inline-block;width:10%;height:100%;vertical-align:top;box-sizing:border-box;">run</button>',
 	'</div>',
-	'<button id="kConsoleShowHideBtn" style="font-size:16px;line-height:24px;position:fixed;top:0;right:0;box-sizing:border-box;z-index:32767;">console</button>',
+	'<div style="font-size:16px;line-height:24px;position:fixed;top:0;right:0;box-sizing:border-box;z-index:32767;">',
+		'<button id="kConsoleClear" style="display:none;">clear</button>',
+		'<button id="kConsoleShowHideBtn">console</button>',
+	'</div>',
 '</div>'].join('')));
 kConsoleLog = function(anything, color){
 	var str = undefined;
@@ -52,10 +55,16 @@ document.getElementById('kConsoleBtnRun').onclick = function(){
 	kConsoleLog(result);
 };
 
+document.getElementById('kConsoleClear').onclick = function(){
+	document.getElementById('kConsoleLogDiv').innerHTML = "";
+};
+
 document.getElementById('kConsoleShowHideBtn').onclick = function(){
 	if($('#kConsoleLogDiv').is(':visible')){
 		$(this).siblings().hide();
+		$(this).parent().siblings().hide();
 	}else{
 		$(this).siblings().show();
+		$(this).parent().siblings().show();
 	}
 };
